ad7abd900718329e06367e7c0a4217c6868a707d
Jerome Haltom <wasabi@cogito.cx>
2017-08-11 14:00:58 -0500

    Initial commit.

---
+++ SpellChecker.Tests/DictionaryDotComSpellCheckerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using SpellChecker.Core;
+
+using SpellChecker.Contracts;
+
+namespace SpellChecker.Tests
+{
+
+    [TestFixture]
+    public class DictionaryDotComSpellCheckerTests
+    {
+
+        ISpellChecker spellChecker;
+
+        [OneTimeSetUp]
+        public void TestFixureSetUp()
+        {
+            spellChecker = new DictionaryDotComSpellChecker();
+        }
+
+        [Test]
+        public void Check_That_FileAndServe_Is_Misspelled()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Check_That_South_Is_Not_Misspelled()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
---
+++ SpellChecker.Tests/MnemonicSpellCheckerIBeforeETests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using SpellChecker.Contracts;
+using SpellChecker.Core;
+
+namespace SpellChecker.Tests
+{
+
+    [TestFixture]
+    public class MnemonicSpellCheckerIBeforeETests
+    {
+
+        ISpellChecker spellChecker;
+
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+            spellChecker = new MnemonicSpellCheckerIBeforeE();
+        }
+
+        [Test]
+        public void Check_Word_That_Contains_I_Before_E_Is_Spelled_Correctly()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Check_Word_That_Contains_I_Before_E_Is_Spelled_Incorrectly()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
