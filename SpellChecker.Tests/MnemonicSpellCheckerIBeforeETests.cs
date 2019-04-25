ab1290ed6ebb274c83696ba55b83a51d0b18ccf6
Enoch Finley <enochfinley@gmail.com>
2019-04-25 13:01:04 -0500

    Implemented MnemonicSpellChecker tests

--- SpellChecker.Tests/MnemonicSpellCheckerIBeforeETests.cs
+++ SpellChecker.Tests/MnemonicSpellCheckerIBeforeETests.cs
@@ -23,13 +23,53 @@ namespace SpellChecker.Tests
         [TestMethod]
         public void Check_Word_That_Contains_I_Before_E_Is_Spelled_Correctly()
         {
-            throw new NotImplementedException();
+            //Arrange
+            var test = "Believe";
+
+            //Act
+            var result = spellChecker.Check(test);
+
+            //Assert
+            Assert.IsTrue(result);
         }
 
         [TestMethod]
-        public void Check_Word_That_Contains_I_Before_E_Is_Spelled_Incorrectly()
+        public void Check_Word_That_Contains_C_Before_E_Before_I_Is_Spelled_Correctly()
         {
-            throw new NotImplementedException();
+            //Arrange
+            var test = "Ceiling";
+
+            //Act
+            var result = spellChecker.Check(test);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Check_Word_That_Contains_E_Before_I_Is_Spelled_Incorrectly()
+        {
+            //Arrange
+            var test = "Protein";
+
+            //Act
+            var result = spellChecker.Check(test);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Check_Word_That_Contains_C_Before_I_Before_E_Is_Spelled_Incorrectly()
+        {
+            //Arrange
+            var test = "Science";
+
+            //Act
+            var result = spellChecker.Check(test);
+
+            //Assert
+            Assert.IsFalse(result);
         }
 
     }
